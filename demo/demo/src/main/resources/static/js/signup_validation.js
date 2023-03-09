e674076b1fa15bb23a3044b59296710a66a6d51f
yj2980 <gum0945@naver.com>
2023-03-10 01:21:20 +0900

    feat : add id & password validation function

--- demo/demo/src/main/resources/static/js/signup_validation.js
+++ demo/demo/src/main/resources/static/js/signup_validation.js
@@ -1,74 +1,128 @@
-function validEmail(obj) {
-    let emailCheckNotice = document.getElementById('email-notice');
-    let emailOverlapButton = document.getElementById('id_overlap_button');
-    let validEmailResult = validEmailCheck(obj);
+const NOTICE = {
+    VALID_EMAIL : "사용가능한 아이디 입니다.",
+    INVALID_EMAIL : "이미 존재하는 아이디 입니다.",
+    INVALID_PW : "비밀번호는 8자 이상이어야 하며, 숫자/대문자/소문자/특수문자를 모두 포함해야 합니다.",
+    CONSECUTIVE_PW : "같은 문자를 4번 이상 사용하실 수 없습니다.",
+    KOREAN_PW : "비밀번호에 한글을 사용 할 수 없습니다.",
+    SPACE_PW : "비밀번호는 공백 없이 입력해주세요.",
+    NOT_SAME_PW : "비밀번호가 일치하지 않습니다.",
+    INPUT_USERNAME : "닉네임을 입력해주세요.",
+    INPUT_EMAIL : "이메일 중복 검사를 확인해주세요.",
+    INPUT_PASSWORD : "비밀번호를 입력해주세요."
+};
 
-    if (validEmailResult) {
-        console.log("email validation is true");
-        emailCheckNotice.style.display = "none";
-        emailOverlapButton.style.display = "block";
-    }
-    if (!validEmailResult) {
-        console.log("email validation is false");
-        emailOverlapButton.style.display = "none";
-        emailCheckNotice.style.display = "block";
-        obj.value = '';
-        obj.focus();
+let isVisited = false;
+
+function verifyEmail(email) {
+    if (isRight(email)) {
+        $("#id_overlap_button").css("display", "none");
+        $("#email-notice").css("display", "block");
+        initInputText(email);
         return false;
     }
+
+    $("#email-notice").css("display", "none");
+    $("#id_overlap_button").css("display", "block");
 }
 
-function validEmailCheck(obj) {
+function isRight(email) {
    var pattern = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
 
-   return (obj.value.match(pattern) != null);
+   return (email.value.match(pattern) == null);
 }
 
-function id_overlap_check() {
-    let userId = document.querySelector('input[name="email"]');
-    console.log("UserEmail : " + userId.value);
+function isEmailDuplicate() {
+    let userEmail = $("#user_email");
+
     $.ajax({
-        url:'./emailCheck',
+        url:'./emailDuplicationCheck',
         type:'post',
         data: {
-           'email': userId.value
+           'email': userEmail.val()
         },
         datatype: 'json',
-        success: function (overlappedEmail) {
-            console.log("email duplication result : " + overlappedEmail)
-            // 사용 가능
-            if (!overlappedEmail) {
-                 alert("사용가능한 아이디 입니다.");
+        success: function (result) {
+            if (!result) {
+                 alert(NOTICE.VALID_EMAIL);
                  $('#id_overlap_button').hide();
+                 isVisited = true;
                  return;
             }
-            if (overlappedEmail) {
-               alert("이미 존재하는 아이디 입니다.");
-               userId.focus();
+            if (result) {
+               alert(NOTICE.INVALID_EMAIL);
                return;
             }
         }
     })
 }
 
-function validPassword(){
-	var pw = $("#user_password").val();
-
+function verifyPassword(pw){
 	var reg = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+	var duplication = /(\w)\1\1\1/;
+	var space = /\s/;
 	var korean = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
 
-	if (!reg.test(pw)) {
-	    alert('비밀번호는 8자 이상이어야 하며, 숫자/대문자/소문자/특수문자를 모두 포함해야 합니다.');
-	}else if (/(\w)\1\1\1/.test(pw)){
-	    alert('같은 문자를 4번 이상 사용하실 수 없습니다.');
+    if (!validPasswordCheck(pw, reg)) {
+        noticeReason(NOTICE.INVALID_PW, pw);
+        return false;
+    }
+    if (validPasswordCheck(pw, duplication)) {
+        noticeReason(NOTICE.CONSECUTIVE_PW, pw);
 	    return false;
-	}else if (pw.search(/\s/) != -1){
-	    alert("비밀번호는 공백 없이 입력해주세요.");
+    }
+    if (validPasswordCheck(pw, space)){
+	    noticeReason(NOTICE.SPACE_PW, pw);
 	    return false;
-	}else if (korean.test(pw)){
-	    alert("비밀번호에 한글을 사용 할 수 없습니다.");
-	}else {
-	 console.log("통과");
 	}
-
+	if (validPasswordCheck(pw, korean)){
+	    noticeReason(NOTICE.KOREAN_PW, pw);
+	    return false;
+	}
 }
+
+function isSamePassword(pw) {
+    if (isNotSame($("#user_password").val(), pw.value)) {
+        noticeReason(NOTICE.NOT_SAME_PW, pw);
+    }
+}
+
+function isNotSame(first, second) {
+    return (first !== second);
+}
+
+
+function validPasswordCheck(obj, pattern) {
+   return (obj.value.match(pattern) != null);
+}
+
+function noticeReason(text, obj) {
+    alert(text);
+    initInputText(obj);
+}
+
+function initInputText(obj) {
+    obj.value = '';
+    obj.focus();
+}
+
+document.getElementById("signup-submit").addEventListener("click", signup);
+
+function signup() {
+    if ($("#username").val() == "") {
+        alert(NOTICE.INPUT_USERNAME);
+        return;
+    }
+
+    if (!isVisited) {
+        alert(NOTICE.INPUT_EMAIL);
+        return;
+    }
+
+    if ($("#user_password").val() == "" || $("#user_correct_password").val() == "") {
+        alert(NOTICE.INPUT_PASSWORD);
+        return;
+    }
+
+    $("#signupForm").submit();
+}
+
---
+++ demo/demo/src/test/java/web/example/demo/SignInTest.java
@@ -0,0 +1,2 @@
+package web.example.demo;public class SignInTest {
+}
