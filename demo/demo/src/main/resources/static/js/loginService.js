595405bfaf4970c6fa0af21581e5d1a203a40c94
yj2980 <gum0945@naver.com>
2023-03-10 01:28:29 +0900

    feat : save user login info in localStorage or SessionStorage

---
+++ demo/demo/src/main/resources/static/js/loginService.js
@@ -0,0 +1,35 @@
+if (sessionStorage.getItem("login") == "true" || localStorage.getItem("login") == "true") {
+    console.log("login success");
+    document.getElementById("navbar_login").style.display = "none";
+    document.getElementById("navbar_mypage").style.display = "block";
+    document.getElementById("navbar_logout").style.display = "block";
+} else {
+    console.log("login false");
+    document.getElementById("navbar_login").style.display = "block";
+    document.getElementById("navbar_mypage").style.display = "none";
+    document.getElementById("navbar_logout").style.display = "none";
+}
+
+if (sessionStorage.getItem("login") == "false" && localStorage.getItem("login") == "false") {
+    console.log("login false");
+    document.getElementById("navbar_login").style.display = "block";
+    document.getElementById("navbar_mypage").style.display = "none";
+    document.getElementById("navbar_logout").style.display = "none";
+}
+
+function logout() {
+   console.log("logout");
+
+   if (sessionStorage.getItem("login")) {
+        console.log("session reset");
+        sessionStorage.setItem("login", "false");
+   }
+   if (localStorage.getItem("login")) {
+        console.log("local reset");
+        localStorage.setItem("login", "false");
+   }
+
+   window.location.href = "./";
+}
+
+
