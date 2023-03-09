69b6966f698d5213b0304b115570175f9dc7fd95
yj2980 <gum0945@naver.com>
2023-03-10 01:29:20 +0900

    test : add sign in testcase

--- demo/demo/src/test/java/web/example/demo/SignInTest.java
+++ demo/demo/src/test/java/web/example/demo/SignInTest.java
@@ -1,2 +1,53 @@
-package web.example.demo;public class SignInTest {
+package web.example.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import web.example.demo.service.UserService;
+
+@SpringBootTest
+public class SignInTest {
+
+	@Autowired
+	private UserService userService;
+	private boolean result;
+	@Test
+	void is_correct() {
+		String email = "adore2100@naver.com";
+		String pw = "";
+
+		if (userService.validationLogin(email, pw).equals("true")) {
+			result = true;
+		}
+
+		assertTrue(result);
+	}
+
+	@Test
+	void not_exist_user() {
+		String email = "cc90@gmail.com";
+		String pw = "ccc";
+
+		if (userService.validationLogin(email, pw).equals("not_exist")) {
+			result = false;
+		}
+
+		assertFalse(result);
+	}
+
+	@Test
+	void not_same_pw() {
+		String email = "adore2100@naver.com";
+		String pw = "ccc";
+
+		if (userService.validationLogin(email, pw).equals("not_same")) {
+			result = false;
+		}
+
+		assertFalse(result);
+	}
+
 }
