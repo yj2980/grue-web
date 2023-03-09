1f1f95908d39b45920479627501909952bece0dc
yj2980 <gum0945@naver.com>
2023-03-10 01:32:33 +0900

    feat : implement sign in

--- demo/demo/src/main/java/web/example/demo/service/UserService.java
+++ demo/demo/src/main/java/web/example/demo/service/UserService.java
@@ -1,6 +1,9 @@
 package web.example.demo.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import web.example.demo.model.User;
@@ -11,13 +14,35 @@ public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public void user_info_save(User user) {
+		String encodedPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
 		userRepository.save(user);
 	}
 
 	public boolean checkEmailDuplicate(String email) {
 		return userRepository.existsByEmail(email);
+	}
+
+	public String validationLogin(String email, String password) {
+		User user_info = findUserByEmail(email);
+
+		if (user_info == null) {
+			return "not_exist";
+		}
+
+		if (!passwordEncoder.matches(password, String.valueOf(user_info.getPassword()))) {
+			return "not_same";
+		}
+
+		return "true";
+	}
+
+	public User findUserByEmail(String email) {
+		return userRepository.findByEmail(email);
 	}
 
 }
