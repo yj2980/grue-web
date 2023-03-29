a4054b883c5a9449a249aab4437809ce1957d89f
yj2980 <gum0945@naver.com>
2023-03-30 01:04:52 +0900

    feat : add time entity

--- demo/demo/src/main/java/web/example/demo/model/Time.java
+++ demo/demo/src/main/java/web/example/demo/model/Time.java
@@ -1,2 +1,27 @@
-package web.example.demo.model;public class Time {
+package web.example.demo.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+public abstract class Time {
+
+	@CreatedDate
+	@Column(updatable = false)
+	private LocalDateTime createDate;
+
+	@LastModifiedDate
+	@Column
+	private LocalDateTime modifiedDate;
 }
