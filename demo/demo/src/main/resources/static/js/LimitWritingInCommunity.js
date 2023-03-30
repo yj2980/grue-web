50871344b4339fbedd788284115b8e0048a015e8
yj2980 <gum0945@naver.com>
2023-03-30 19:52:51 +0900

    feat: Implement edit/delete feature for post authors only

--- demo/demo/src/main/resources/static/js/LimitWritingInCommunity.js
+++ demo/demo/src/main/resources/static/js/LimitWritingInCommunity.js
@@ -1,7 +1,9 @@
 
 // 로그인한 사람이 아닌 경우 글쓰기 제한
-if (sessionStorage.getItem("username") == null || localStorage.getItem("username") == null) {
-    $("#btn-primary").css("display", "none");
+if (sessionStorage.getItem("username") == null && localStorage.getItem("username") == null) {
+    console.log("display is none a button");
+    $("#btn-write").css("display", "none");
 } else {
-    $("#btn-primary").css("display", "block");
+    console.log("display a button")
+    $("#btn-write").css("display", "block");
 }
--- demo/demo/src/main/resources/static/js/preventEditNotAuthor.js
+++ demo/demo/src/main/resources/static/js/preventEditNotAuthor.js
@@ -1,10 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>$Title$</title>
-</head>
-<body>
-$END$
-</body>
-</html>
+var username = "";
+
+if (sessionStorage.getItem("username")) {
+    console.log("session username : " + sessionStorage.getItem("username"));
+    username = sessionStorage.getItem("username");
+}
+if (localStorage.getItem("username")) {
+    console.log("local username : " + localStorage.getItem("username"));
+    username = localStorage.getItem("username");
+}
+
+if (username == postAuthor) {
+    $("#edit-btn").css("display", "block");
+    $("#delete-btn").css("display", "block");
+} else {
+    $("#edit-btn").css("display", "none");
+    $("#delete-btn").css("display", "none");
+}
