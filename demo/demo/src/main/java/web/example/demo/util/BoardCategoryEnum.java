904c4972d05bcef07f90829dae352964f78e3a22
yj2980 <gum0945@naver.com>
2023-03-30 19:50:43 +0900

    feat : add board category enum class

--- demo/demo/src/main/java/web/example/demo/util/BoardCategoryEnum.java
+++ demo/demo/src/main/java/web/example/demo/util/BoardCategoryEnum.java
@@ -1,2 +1,36 @@
-package web.example.demo.util;public enum BoardCategoryEnum {
+package web.example.demo.util;
+
+import java.util.Arrays;
+
+public enum BoardCategoryEnum {
+	UNKNOWN("unknown", "unknown"),
+	GENERAL("general_board", "자유"),
+	QUESTION("question_board", "질문"),
+	REVIEW("review_board", "후기"),
+	SHARING("sharingInformation_board", "정보공유");
+
+	private String selectValue;
+	private String categoryValue;
+
+	BoardCategoryEnum(String selectValue, String categoryValue) {
+		this.selectValue = selectValue;
+		this.categoryValue = categoryValue;
+	}
+
+	public String getSelectValue() {
+		return selectValue;
+	}
+
+	public String getCategoryValue() {
+		return categoryValue;
+	}
+
+
+	public static String findCategoryBySelectValue(String select) {
+		return Arrays.stream(values())
+				.filter(v -> v.selectValue.equals(select))
+				.findAny()
+				.orElse(UNKNOWN)
+				.getCategoryValue();
+	}
 }
